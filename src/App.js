daab6c035a95aabfee56acc0f8ccb297cdecb015
kashif ullah <43569363+kaashe@users.noreply.github.com>
2022-08-09 11:52:22 +0500

    Template

--- src/App.js
+++ src/App.js
@@ -49,39 +49,102 @@ function App() {
             <img src='gold.png' alt='img not found'></img>
           </div>
         </div>
-        <div className='nft-collection'> 
+        <div className='nft-collection'>
           <h1>NFT COLLECTION</h1>
-            <h3>10,000 Unique Sloths That Will Animate In Our Game With Many Perks, Accessories & Upgrades Available</h3>
+          <h3>10,000 Unique Sloths That Will Animate In Our Game With Many Perks, Accessories & Upgrades Available</h3>
           <div className='nft-collection-card'>
-          <img src='sloth-v1.jpeg' alt='No img found'></img>
-          <img src='sloth-v2.jpeg' alt='No img found'></img>
-          <img src='sloth-v3.jpeg' alt='No img found'></img>
-          <img src='sloth-v4.jpeg' alt='No img found'></img>
+            <img src='sloth-v1.jpeg' alt='No img found'></img>
+            <img src='sloth-v2.jpeg' alt='No img found'></img>
+            <img src='sloth-v3.jpeg' alt='No img found'></img>
+            <img src='sloth-v4.jpeg' alt='No img found'></img>
           </div>
         </div>
         <div className='tokenomics'>
           <h1>TOKENOMICS</h1>
-        <div className='tokenomicsItems'> 
-          <img src='chart.png' alt='No img found'></img>
-          <div className='tokenomicsList'>
-            <h3>Play to Earn $Fantasy Cryptocurrency</h3>
-            <ul>
-              <li>ERC-20 Token on Ethereum Blockchain</li>
-              <li>Daily $Fantasy Income</li>
-              <li>BSC Metaverse Business</li>
-              <li>Stock/Shares Acquiring</li>
-              <li>LAND Purchase WL for LAND Minting</li>
-              <li>NFT Staking for Additional Income </li>
-              <li>NFT Character Trait Upgrades</li>
-              <li>In Game Cosmetics/Assets & Perks Development</li>
-            </ul>
-            <button>WHITEPAPER</button>
+          <div className='tokenomicsItems'>
+            <img src='chart.png' alt='No img found'></img>
+            <div className='tokenomicsList'>
+              <h3>Play to Earn $Fantasy Cryptocurrency</h3>
+              <ul>
+                <li>ERC-20 Token on Ethereum Blockchain</li>
+                <li>Daily $Fantasy Income</li>
+                <li>BSC Metaverse Business</li>
+                <li>Stock/Shares Acquiring</li>
+                <li>LAND Purchase WL for LAND Minting</li>
+                <li>NFT Staking for Additional Income </li>
+                <li>NFT Character Trait Upgrades</li>
+                <li>In Game Cosmetics/Assets and Perks Development</li>
+              </ul>
+              <button>WHITEPAPER</button>
+            </div>
           </div>
         </div>
-        </div>
         <div className='roadmap'>
-        <h1>ROADMAP</h1>
-        </div>
+          <h1>ROADMAP</h1>
+          <div className='stage1'>
+            <h3>Stage 1  | Community Building</h3>
+            <h4>Refferal Program</h4>
+            <div>
+              <p>
+                Our referral program is truly the first of its kind in the NFT space!
+                Our utility starts the moment you are approved for our invite only
+                Discord! You’ll be granted 100 $Fantasy upon entering our VIP community
+                Each $Fantasy is currently valued at $.10 ****VERY IMPORTANT**** You can not
+                access these tokens unless you own the NFT! There is no cap on how many tokens you can earn.
+
+                So how do you earn the $Fantasy? Pre Mint, you can acquire tokens in 2 ways
+                . The first way is through being active and leveling up in the discord.The second way is by inspiring and referring friends, family, colleagues, and others to join our VIP community. For every person you bring to the discord, you will have the opportunity earn another 100 $Fantasy. With your help we are building the best and brightest community in the NFT space.
+
+                After minting your Baby Sloth Club NFT through out website or buying it on OpenSeayou can immediately be able to start staking your NFT. You can also earn $Fantasy by grinding our P2E game as you will get rewarded for every level you pass. Sharing your game progress on social media will grant you bonus tokens as well! Stay tuned for the game.
+
+                We will also be hosting many giveaways pre and post mint so make sure to tune in for that too.
+                <h4>Mint</h4>
+                Mint day is where our precious Baby Sloth holders will be able to claim their $Fantasy, and start staking. Mark your calendars because you won’t want to miss out on this one, and understand that the mint is just the beginning! We’ve got many things planned. Stay tuned for more information.
+              </p>
+            </div>
+          </div>{/* STAGE 1 END */}
+          <div className='stage2'>
+            <h3>Stage 2  |  $Fantasy Launch</h3>
+            <h4>$FANTASY Launch Immediately Post Mint</h4>
+            <p>
+              o all of our future Baby Sloth holders, here is where you will receive all the $Fantasy you have accumulated so far. Everything is unlocked for you and the sky's the limit in terms of opportunities on your sloth journey!
+            </p>
+            <h4>Staking and Utility</h4>
+            <p>
+              That’s right! After mint you’ll be able to officially start staking your NFT. You can earn up to 200 $Fantasy a day. You can use these tokens to increase the rarity of your sloth. We’re not kidding! So here’s how it’s gonna work. For every rarity, the duration of your staking term will be different. The longer you let your sloth explore the wildlife, the more valuable it will come back to you.
+            </p>
+
+            <div className='timediv'>
+              <h4>Here are the times for each rarity:</h4>
+              <p>Common  Uncommon : 1 month <br></br>
+                Uncommon  Rare: 2 months   <br></br>
+                Rare  Epic: 3 months  <br></br>
+                Epic Legendary: 4 months
+              </p>
+            </div>
+            <p>During the time that your sloth is exploring the wild, be sure to engage in our P2E game (coming soon), as well as continue grinding in the discord to accumulate more $Fantasy!</p>
+            <h4>Store</h4>
+            <p>
+              Your $Fantasy Tokens can also be used in our VIP store. Use your $Fantasy to buy exclusive Baby Sloth Club Merch, physical collectables, in game boosts, as well as access to our exclusive VIP meetups and events
+            </p>
+
+          </div>
+          <div className='stage3'>
+            <h3>Stage 3  |  P2E</h3>
+            <h4>P2E Game and Metaverse</h4>
+            <p>Our NFT holders will have access to our own exclusive game in the Metaverse. Our vision for this game is for our community to have fun while exploring Web3.0. You’ll have your own Sloth to play with as a character and your objective will be to take care of your sloth and nurture him in life. You get to explore and decide which career path you want your sloth to take. After completing each task that’s presented to you, you’ll earn $Fantasy. A sloth that completes the game, will be solidified as a more valuable Baby Sloth NFT. We encourage you to play the game again as the second time, your sloth will now be earning you double $Fantasy. Not only will you have a great time in this game, we made sure that investing your time benefits you a huge return.</p>
+          </div>
+        </div>    
+       {/*roadmap continue,one new div for background change only */}
+       <div className='rodamapContinue'>
+         <h1>TEAM</h1>
+         <div className='teamItems'>
+           <div className='taemcard'>
+           <img src='baby.png' alt='img not found'></img>
+           </div>
+        
+         </div>
+      </div> 
       </header>
     </div>
   );
